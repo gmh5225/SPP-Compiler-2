1e9b8a8c632de046a0fe2f18602491c2417b7688
Sam <samuelgardner101@gmail.com>
2023-06-25 22:22:19 +0100

    Experimentation

--- src/CodeGen.py
+++ src/CodeGen.py
@@ -1,19 +1,33 @@
-import llvmlite.ir as llvm
+import llvmlite.ir as ll
+import llvmlite.binding as llvm
 
 from src.Ast import *
 
 
 class CodeGen:
     def __init__(self):
-        self.context = llvm.Context()
-        self.module = llvm.Module(name="main", context=self.context)
-        self.builder = llvm.IRBuilder(self.context)
+        self.context = ll.Context()
+        self.module = ll.Module(name="main", context=self.context)
+        self.builder = ll.IRBuilder(self.context)
 
-    def generate_function(self, llvm_function: FunctionPrototypeAst) -> llvm.FunctionType:
-        llvm_function_type = llvm.FunctionType(self.generate_type(llvm_function.return_type), self.generate_parameters(llvm_function.parameters), llvm_function.parameters[-1].is_variadic)
-        llvm_function = llvm.Function(self.module, llvm_function_type, name=llvm_function.identifier)
+        llvm.initialize()
+        llvm.initialize_native_target()
+        llvm.initialize_native_asmprinter()
 
-        block = llvm_function.append_basic_block(name="entry")
-        self.builder.position_at_end(block)
+    def _generate_llvm_function(self, ast: FunctionPrototypeAst) -> ll.Function:
+        is_variadic = len(ast.parameters) and ast.parameters[-1].is_variadic
+        llvm_function_return_type = self._generate_llvm_type(ast.return_type)
+        llvm_function_parameter_types = self._generate_parameter_types(ast.parameters)
+        llvm_function_type = ll.FunctionType(llvm_function_return_type, llvm_function_parameter_types, is_variadic)
 
+        llvm_function = ll.Function(self.module, llvm_function_type, name=ast.identifier)
+        llvm_function.linkage = self._generate_llvm_linkage(ast.modifier)
 
+        llvm_basic_block_entry = llvm_function.append_basic_block(name="entry")
+        llvm_builder = ll.IRBuilder()
+        llvm_builder.position_at_end(llvm_basic_block_entry)
+        llvm_basic_blocks_body = [self._generate_llvm_block_from_statement(llvm_builder, statement) for statement in ast.body]
+        llvm_basic_block_exit = llvm_builder.append_basic_block(name="exit")
+        llvm_builder.position_at_end(llvm_basic_block_exit)
+
+        return llvm_function
