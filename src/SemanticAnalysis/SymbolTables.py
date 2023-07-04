81464c212ce9c9463c9048c728e175da3ae1c149
Sam <samuelgardner101@gmail.com>
2023-07-04 13:57:19 +0100

    Symbol Table Generation

--- src/SemanticAnalysis/SymbolTables.py
+++ src/SemanticAnalysis/SymbolTables.py
@@ -104,22 +104,94 @@ class SymbolTableGenerator:
     """
     Generate a symbol table for each scope. This is used for type checking and variable declaration checking. For each
     statement that can introduce a scope, a new symbol table is created. This symbol table is then attached to the
-    parent symbol statement, to allow for upwards searching of symbols.
+    parent symbol statement, to allow for upwards searching of symbols. This class just manages the symbols, not
+    checking if types or loop tags actually exist. No analysis happens here, just the building of the symbol tables.
     """
 
     @staticmethod
     @multimethod
     @nest_next_scope
-    def analyse(ast: Ast.ProgramAst, manager: SymbolTableManager) -> None:
+    def build_symbols(ast: Ast.ProgramAst, manager: SymbolTableManager) -> None:
         for statement in ast.module.body.members:
-            SymbolTableGenerator.analyse(statement, manager)
+            SymbolTableGenerator.build_symbols(statement, manager)
 
     @staticmethod
     @multimethod
     @nest_next_scope
-    def analyse(ast: Ast.FunctionPrototypeAst, manager: SymbolTableManager) -> None:
+    def build_symbols(ast: Ast.FunctionPrototypeAst, manager: SymbolTableManager) -> None:
         for parameter in ast.parameters:
-            SymbolTableGenerator.analyse(parameter, manager)
+            SymbolTableGenerator.build_symbols(parameter, manager)
+        for statement in ast.body.statements:
+            SymbolTableGenerator.build_symbols(statement, manager)
 
+    @staticmethod
+    @multimethod
+    @nest_next_scope
+    def build_symbols(ast: Ast.IfStatementAst, manager: SymbolTableManager) -> None:
+        SymbolTableGenerator.build_symbols(ast.if_branch, manager)
+        for branch in ast.elif_branches:
+            SymbolTableGenerator.build_symbols(branch, manager)
+        if ast.else_branch:
+            SymbolTableGenerator.build_symbols(ast.else_branch, manager)
 
+    @staticmethod
+    @multimethod
+    @nest_next_scope
+    def build_symbols(ast: Ast.IfStatementBranchAst | Ast.ElifStatementBranchAst | Ast.ElseStatementBranchAst, manager: SymbolTableManager) -> None:
+        # Else branch will never have any definitions (parser forces this)
+        for definition in ast.definitions:
+            SymbolTableGenerator.build_symbols(definition, manager)
+        for statement in ast.body:
+            SymbolTableGenerator.build_symbols(statement, manager)
 
+    @staticmethod
+    @multimethod
+    @nest_next_scope
+    def build_symbols(ast: Ast.WhileStatementAst, manager: SymbolTableManager) -> None:
+        for statement in ast.body:
+            SymbolTableGenerator.build_symbols(statement, manager)
+
+    @staticmethod
+    @multimethod
+    @nest_next_scope
+    def build_symbols(ast: Ast.ForStatementAst, manager: SymbolTableManager) -> None:
+        for identifier in ast.identifiers:
+            SymbolTableGenerator.build_symbols(identifier, ast.iterable, manager)
+        for statement in ast.body:
+            SymbolTableGenerator.build_symbols(statement, manager)
+            
+    @staticmethod
+    @multimethod
+    @nest_next_scope
+    def build_symbols(ast: Ast.DoWhileStatementAst, manager: SymbolTableManager) -> None:
+        for statement in ast.body:
+            SymbolTableGenerator.build_symbols(statement, manager)
+        
+    @staticmethod
+    @multimethod
+    @nest_next_scope
+    def build_symbols(ast: Ast.MatchStatementAst, manager: SymbolTableManager) -> None:
+        for case in ast.cases:
+            SymbolTableGenerator.build_symbols(case, manager)
+        
+    @staticmethod
+    @multimethod
+    @nest_next_scope
+    def build_symbols(ast: Ast.CaseStatementAst, manager: SymbolTableManager) -> None:
+        for statement in ast.body:
+            SymbolTableGenerator.build_symbols(statement, manager)
+
+    @staticmethod
+    @multimethod
+    @nest_next_scope
+    def build_symbols(ast: Ast.WithStatementAst, manager: SymbolTableManager) -> None:
+        SymbolTableGenerator.build_symbols(ast.alias, manager)
+        for statement in ast.body:
+            SymbolTableGenerator.build_symbols(statement, manager)
+
+    @staticmethod
+    @multimethod
+    @nest_next_scope
+    def build_symbols(ast: Ast.InnerScopeAst, manager: SymbolTableManager) -> None:
+        for statement in ast.body:
+            SymbolTableGenerator.build_symbols(statement, manager)
